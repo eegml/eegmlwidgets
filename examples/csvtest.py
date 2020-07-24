70317c87ec954f116d128d6768e0142d84d31b23
chris lee-messer <chris@lee-messer.net>
2020-07-24 16:38:41 -0700

    update example

---
+++ examples/csvtest.py
@@ -0,0 +1,56 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent,md
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: pytorch 1.5+
+#     language: python
+#     name: pyt15
+# ---
+
+# %% [markdown]
+# ### CsvGridWidget
+# CsvGridWidget is meant to be a quick way to visualize and edit a csv file using pandas, ipywidgets and qgrid
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import eegmlwidgets                                             
+                                                       
+eegmlwidgets.wide_notebook_display()   # allow jupyternotebook to use full width      
+
+# %%
+datacsv = eegmlwidgets.CsvGridWidget("SalesJan2009.csv")
+
+# %%
+datacsv.show() # doupble click a cell to make changes, click save to save the grid state back to the csv file, note if you have filtered, this will delete data!
+
+# %% [markdown]
+# To manipulate the underlying dataframe directly, get the DataFrame from
+# datacsv.df
+
+# %%
+# example
+datacsv.df.head()
+
+# %%
+
+# %%
+import qgrid
+qgrid.__version__
+
+# %%
+import ipywidgets
+ipywidgets.__version__
+
+# %%
+# thanks to this article for the source of the example csv:
+# source url https://support.spatialkey.com/spatialkey-sample-csv-data/ 
+# # # !wget http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv 
